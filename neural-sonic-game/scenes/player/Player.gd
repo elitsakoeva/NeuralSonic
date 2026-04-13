ad3bc2465ca1d9fb83be4388243fc67a12c15592
Elitsa Koeva <elioneli1888@gmail.com>
2026-04-13 12:32:37 +0300

    adding a simple asset and debugging player (using default movement script)

--- neural-sonic-game/scenes/player/Player.gd
+++ neural-sonic-game/scenes/player/Player.gd
@@ -1,23 +1,26 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = - 350.0
-const ACCELERATION = 10.0
-const FRICTION = 8.0
-const GRAVITY = 900.0
 
-func _physics_process(delta):
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		velocity += get_gravity() * delta
 
+	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
-		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION)
-
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
