6d3fefde8fc9ffdb9c2b7a5f39e2bf7ff55aef05
Elitsa Koeva <elioneli1888@gmail.com>
2026-04-18 23:20:40 +0300

    timer in sonic style

--- neural-sonic-game/scenes/ui/HUD.gd
+++ neural-sonic-game/scenes/ui/HUD.gd
@@ -1,11 +1,24 @@
-extends Node
+extends CanvasLayer
 
+@onready var time_label = $TimeLabel
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var elapsed_time: float = 0.0
+var is_running: bool = true
 
+func _process(delta):
+	if not is_running:
+		return
+	
+	elapsed_time += delta
+	
+	var minutes = int(elapsed_time) / 60
+	var seconds = int(elapsed_time) % 60
+	
+	time_label.text = "TIME: %02d:%02d" % [minutes, seconds]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func stop_timer():
+	is_running = false
+
+func reset_timer():
+	elapsed_time = 0.0
+	is_running = true
