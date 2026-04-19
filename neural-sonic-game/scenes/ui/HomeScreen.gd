cc20b696fdd2203a77a6770fc49c065fa088961e
Elitsa Koeva <elioneli1888@gmail.com>
2026-04-19 15:14:18 +0300

    adding exit btn and making changes in Home screen

--- neural-sonic-game/scenes/ui/HomeScreen.gd
+++ neural-sonic-game/scenes/ui/HomeScreen.gd
@@ -2,13 +2,13 @@ extends Control
 
 @onready var play_button = $VBoxContainer/Play
 @onready var ai_button = $VBoxContainer/AIMode
-@onready var settings_button = $VBoxContainer/Settings
+@onready var exit_button = $VBoxContainer/Exit
 
 
 func _ready():
 	play_button.pressed.connect(_on_play_pressed)
 	ai_button.pressed.connect(_on_ai_pressed)
-	settings_button.pressed.connect(_on_settings_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
 
 func _on_play_pressed():
 	GameManager.is_ai_mode = false
@@ -18,5 +18,5 @@ func _on_ai_pressed():
 	GameManager.is_ai_mode = true
 	get_tree().change_scene_to_file("res://scenes/levels/Level1.tscn")
 
-func _on_settings_pressed():
-	pass
+func _on_exit_pressed():
+	get_tree().quit()
