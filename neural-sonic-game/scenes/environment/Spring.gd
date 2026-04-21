5c47809a7cb9cf6fd7572f7f5ba988c6908d30ba
Elitsa Koeva <elioneli1888@gmail.com>
2026-04-21 23:54:37 +0300

    fixing spring animation

--- neural-sonic-game/scenes/environment/Spring.gd
+++ neural-sonic-game/scenes/environment/Spring.gd
@@ -1,14 +1,20 @@
 extends Area2D
 
 const SPRING_FORCE = -600.0
+var is_bouncing = false
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 	$AnimatedSprite2D.play("idle")
+	$AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
 
 func _on_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player" and not is_bouncing:
+		is_bouncing = true
 		body.velocity.y = SPRING_FORCE
 		$AnimatedSprite2D.play("bounce")
-		await $AnimatedSprite2D.animation_finished
+
+func _on_animation_finished():
+	if $AnimatedSprite2D.animation == "bounce":
 		$AnimatedSprite2D.play("idle")
+		is_bouncing = false
