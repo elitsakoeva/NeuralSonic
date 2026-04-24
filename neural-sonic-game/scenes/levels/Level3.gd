264a74ec77d208a85b133197ad0022b8d4873124
Elitsa Koeva <elioneli1888@gmail.com>
2026-04-24 11:24:55 +0300

    linking AI mode and Python script for training

--- neural-sonic-game/scenes/levels/Level3.gd
+++ neural-sonic-game/scenes/levels/Level3.gd
@@ -6,10 +6,15 @@ func _ready():
 	GameManager.rings = 0
 	
 	if GameManager.is_ai_mode:
+		var sync = Node.new()
+		sync.set_script(load("res://addons/godot_rl_agents/sync.gd"))
+		sync.name = "Sync"
+		add_child(sync)
 		$Player/AIController2D.control_mode = AIController2D.ControlModes.TRAINING
 		$Player.visible = true
 		return
 	
+
 	$Player/AIController2D.control_mode = AIController2D.ControlModes.HUMAN
 	
 	if not GameManager.is_reloading:
