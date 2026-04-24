1a958e5d53953e54577c1092e812d2a757228fec
Elitsa Koeva <elioneli1888@gmail.com>
2026-04-24 09:16:45 +0300

    fixing bug with Goal in Player mode

--- neural-sonic-game/scenes/environment/Goal.gd
+++ neural-sonic-game/scenes/environment/Goal.gd
@@ -6,15 +6,15 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	if body.name == "Player":
+	if body is CharacterBody2D:
 		var ai = body.get_node_or_null("AIController2D")
-		if ai:
+		if ai and GameManager.is_ai_mode:
 			ai.reward += 1000.0
 			ai.done = true
-			body.position = Vector2(100, 200)
+			body.position = Vector2(0, 30)
 			body.velocity = Vector2.ZERO
 			return
-
+	   
 		if next_level == "":
 			get_tree().change_scene_to_file("res://scenes/ui/FinalScreen.tscn")
 		else:
--- neural-sonic-game/scenes/levels/Level3.gd
+++ neural-sonic-game/scenes/levels/Level3.gd
@@ -6,11 +6,12 @@ func _ready():
 	GameManager.rings = 0
 	
 	if GameManager.is_ai_mode:
+		$Sync.control_mode = Sync.ControlModes.TRAINING
 		var ai = $Player/AIController2D
 		if ai:
 			ai.control_mode = AIController2D.ControlModes.TRAINING
 	else:
-		GameManager.is_ai_mode = false
+		$Sync.control_mode = Sync.ControlModes.HUMAN
 		var transition = preload("res://scenes/ui/ZoneTransition.tscn").instantiate()
 		add_child(transition)
-		await transition.show_transition("CIAN ZONE", "ACT 3")
+		await transition.show_transition("CYAN ZONE", "ACT 1")
